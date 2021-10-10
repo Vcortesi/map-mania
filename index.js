5b2270b02b467c899d1af3be65c063f4ca637127
Vincent Cortesi <vincentcortesi@gmail.com>
2021-10-10 16:03:07 -0500

    Added header to source code, cleaned up some stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let map;
 
 function initMap() {
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
@@ -47,6 +48,7 @@ function updateGame(){
   }
 
 }
+
 function initApplication() {
   console.log("Map Mania Starting Up!")
 }
